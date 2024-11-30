500bc18b33a8e50088d4c57f07f96e952a22f8de
Sodiya Tobiloba <75803621+Tobi153@users.noreply.github.com>
2024-11-30 01:30:33 +0100

    Added Course Preview to the Dashboard Page

--- src/components/CoursePreview/CoursePreview.jsx
+++ src/components/CoursePreview/CoursePreview.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import './CoursePreview.css';
+import editpdf from '/src/assets/edit-pdf.svg';
+import addpdf from '/src/assets/add-pdf.svg';
+import pdficon from '/src/assets/pdficon.png';
+import PropTypes from 'prop-types';
 
-const CoursePreview = () => {
+const CoursePreview = ({ onClick }) => {
   return (
-    <div className="CoursePreview">
-      <p>Some Text here sha</p>
+    <div className="CoursePreview" onClick={onClick}>
+      <div className="course-preview-container">
+        <div className="course-header">
+          <div className="course-details">
+            <h2 className="course-preview-code">CSC 201: Computer Science</h2>
+            <p className="course-date">12th November, 2024</p>
+          </div>
+          <div className="course-icons">
+            <img src={editpdf} alt="Edit pdf icon" />
+            <img src={addpdf} alt="Add pdf icon" />
+          </div>
+        </div>
+        {Array.from({ length: 5 }, (_, i) => (
+          <div className="course-preview-pdfs" key={i}>
+            <img src={pdficon} alt="pdf" />
+            <div className="course-pdf-details">
+              <p className="course-preview-title">
+                Introduction to Computer Programming.pdf
+              </p>
+              <p className="course-pdf-size">12.2Mb</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
+};
+
+CoursePreview.propTypes = {
+  onClick: PropTypes.func.isRequired,
 };
 
 export default CoursePreview;
--- src/pages/NotepadPage/NotepadPage.jsx
+++ src/pages/NotepadPage/NotepadPage.jsx
@@ -1,8 +1,20 @@
-import React from "react";
-import "./NotepadPage.css";
+import React from 'react';
+import './NotepadPage.css';
 
 const NotepadPage = () => {
-  return <div className="NotepadPage">NotepadPage</div>;
+  return (
+    <div className="NotepadPage">
+      <div className="notepad-title">Notes for CSC 201</div>
+      <ul className="notepad-list">
+        {Array.from({ length: 7 }, (_, i) => (
+          <li className="notepad-point" key={i}>
+            Computer Programming is defined as the process of writing computer
+            programs.
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default NotepadPage;
